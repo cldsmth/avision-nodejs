de642f8c2c58fc087f57fcc717c54ccd1d6418d9
Cliff Dean <cliff.dean@eannovate.com>
2019-03-25 16:36:22 +0700

    clean code admin model js

--- api/models/adminModel.js
+++ api/models/adminModel.js
@@ -2,49 +2,49 @@
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
 	AdminSchema = new Schema({
-	name: {
-		type: String,
-		unique: true,
-		required: "Kindly enter the name of the admin"
-	},
-	email: {
-		type: String,
-		unique: true,
-		required: "E-mail is required"
-	},
-	password: {
-		type: String,
-		default: ""
-	},
-	salt_hash: {
-		type: String,
-		default: ""
-	},
-	reset_code: {
-		type: String,
-		default: ""
-	},
-	auth_code: {
-		type: String,
-		unique: true,
-		required: "Authentication Code is required"
-	},
-	img: {
-		type: String,
-		default: ""
-	},
-	status: {
-		type: Number,
-		default: 0
-	},
-	timestamp: {
-		type: Date,
-		default: Date.now
-	},
-	datetime: {
-		type: Date,
-		default: Date.now
-	}
-});
+		name: {
+			type: String,
+			unique: true,
+			required: "Kindly enter the name of the admin"
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: "E-mail is required"
+		},
+		password: {
+			type: String,
+			default: ""
+		},
+		salt_hash: {
+			type: String,
+			default: ""
+		},
+		reset_code: {
+			type: String,
+			default: ""
+		},
+		auth_code: {
+			type: String,
+			unique: true,
+			required: "Authentication Code is required"
+		},
+		img: {
+			type: String,
+			default: ""
+		},
+		status: {
+			type: Number,
+			default: 0
+		},
+		timestamp: {
+			type: Date,
+			default: Date.now
+		},
+		datetime: {
+			type: Date,
+			default: Date.now
+		}
+	});
 
 module.exports = mongoose.model('admins', AdminSchema);
